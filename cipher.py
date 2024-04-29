de71bf56016572931bbb3bba88fe01956c9cd12d
LukasWalker2 <lukasogwalker@gmail.com>
2024-04-29 23:26:28 +0200

    numkey min size now 256

--- cipher.py
+++ cipher.py
@@ -1,4 +1,4 @@
-def cipher_gen(seed: int) -> int:
+def cypher_gen(seed: int):
     length = len(format(seed, 'b'))
     possible_permutations = 2 ** length
     count = 0
@@ -8,25 +8,25 @@ def cipher_gen(seed: int) -> int:
         seed = seed >> 1 | new_bit << (length - 1)
         yield seed & 1
 
-def crypt(key: str, to_crypt: bytes, min_ciphers: int, extra_ciphers: int) -> bytes:
-    num_key = int.from_bytes(bytes(key, 'utf-8'))
-    ciphers = [cipher_gen(num_key)]
+def crypt(key: str, to_crypt: bytes, min_cyphers: int, extra_cyphers: int) -> bytes:
+    num_key = int.from_bytes(bytes(key, 'utf-8')) + 256
+    cyphers = [cypher_gen(num_key)]
     
-    additional_ciphers = int(''.join([str(next(ciphers[-1])) for _ in range(256)]), 2) % (extra_ciphers + 1)
+    additional_cyphers = int(''.join([str(next(cyphers[-1])) for _ in range(256)]), 2) % (extra_cyphers + 1)
     
-    for _ in range(min_ciphers + additional_ciphers):
-        new_seed = int(''.join([str(next(ciphers[-1])) for _ in range(256)]), 2)
-        ciphers.append(cipher_gen(new_seed))
+    for _ in range(min_cyphers + additional_cyphers):
+        new_seed = int(''.join([str(next(cyphers[-1])) for _ in range(256)]), 2)
+        cyphers.append(cypher_gen(new_seed))
     
-    del ciphers[0]
-    print(f'{len(ciphers)} ciphers used')
+    del cyphers[0]
+    print(f'[CYPHER] used {len(cyphers)} cyphers.')
     message = b''
     for byte in to_crypt:
         num = ''
         for _ in range(8):
             xor_result = 0
-            for cipher in ciphers:
-                xor_result ^= next(cipher)
+            for cypher in cyphers:
+                xor_result ^= next(cypher)
             num += str(xor_result)
         message += int.to_bytes(int(num,2) ^ byte)
     
